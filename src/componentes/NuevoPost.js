3d00a0627053c088995c34a242fb19b1d137002c
emeery <gerardoemery@live.com.mx>
2018-06-21 16:49:57 -0500

    texto ayuda

--- src/componentes/NuevoPost.js
+++ src/componentes/NuevoPost.js
@@ -16,46 +16,64 @@ class NuevoPost extends React.Component {
             handleSubmit } = this.props;
             // meta.touched : boolean
         return (
-            <form onSubmit={
+            <form className="container"  
+                onSubmit={
                 handleSubmit(this.onSubmit.bind(this))}>
                 <h3>Crea un nuevo Post.</h3>
                     
-                    
+                
                     <Field name="titulo" component={ field =>
                         <div className='form-group'>
                         <h4>Titulo</h4>
                         <input {...field.input} 
                         type="text" 
-                        className="form-control"
+                        className="form-control is-valid" 
                         />
-                            <div className="text-help">
-                            {field.meta.touched&&field.meta.error ? field.meta.error : ''}
+                            <div 
+                            className="text-help"
+                            >
+                            <p
+                            style={{color:'grey', fontSize:'15px'}}
+                            >{field.meta.touched&&field.meta.error ? 
+                                field.meta.error : ''}</p>
                             </div>
                         </div>
                     } />
 
                     <Field name="categoria" component={ field =>
-                        <div className='form-group'>
-                        <h4>Categoria</h4>
+                        <div className='form-group'
+                        
+                        >
+                        <h5>Categoria</h5>
                         <input {...field.input} 
                         type="text" 
                         className="form-control"
                         />
-                            <div className="text-help">
-                            {field.meta.touched&&field.meta.error ? field.meta.error : ''}
+                            <div 
+                            className="text-help"
+                            >
+                            <p
+                            style={{color:'grey', fontSize:'15px'}}
+                            >{field.meta.touched&&field.meta.error ? 
+                                field.meta.error : ''}</p>
                             </div>
                         </div>
                     } />
 
                     <Field name="contenido" component={ field =>
                         <div className='form-group'>
-                        <h4>Contenido</h4>
+                        <h5>Contenido</h5>
                         <textarea {...field.input} 
                         type="text" 
                         className="form-control"
                         />
-                            <div className="text-help">
-                            {field.meta.touched&&field.meta.error ? field.meta.error : ''}
+                            <div 
+                            className="text-help"
+                            >
+                            <p
+                            style={{color:'grey', fontSize:'15px'}}
+                            >{field.meta.touched&&field.meta.error ? 
+                                field.meta.error : ''}</p>
                             </div>
                         </div>
                     } />
@@ -64,7 +82,8 @@ class NuevoPost extends React.Component {
                 type="submit" 
                 className="btn btn-info"
                 >Postear</button>
-
+                
+                
             </form>
         )
     }
@@ -91,3 +110,5 @@ export default reduxForm({
  fields: ['titulo', 'categoria', 'contenido'], // campos redux-form
  validate // valida la entrada 
 })(connect(null, { crearPost })(NuevoPost));
+
+
